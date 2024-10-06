a9f0f68b6e47ebfd55ac7bfa90db69edbf7149cf
saurabhdhingra <saurabh20work@gmail.com>
2024-10-06 15:27:34 +0530

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Saurabh Dhingra on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bucket ListTests/Bucket_ListTests.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListTests.swift
+//  Bucket ListTests
+//
+//  Created by Saurabh Dhingra on 06/10/24.
+//
+
+import Testing
+@testable import Bucket_List
+
+struct Bucket_ListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
